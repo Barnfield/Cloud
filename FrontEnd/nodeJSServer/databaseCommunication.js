49eba25a75e416d0c4e89a0e75541ee3df26118a
viktorkaslikSchneider <42968549+viktorkaslikSchneider@users.noreply.github.com>
2018-10-10 10:13:14 +0100

    Create databaseCommunication.js

---
+++ FrontEnd/nodeJSServer/databaseCommunication.js
@@ -0,0 +1,43 @@
+var http = require('http');
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express()
+var path = require('path');
+//const { Pool, Client } = require('pg')
+var async = require('async');
+const pg = require('pg')
+const client = new pg.Client();
+
+
+const pool = new pg.Pool()
+
+
+///////////////DB QUERY////////////////////////////
+	//Insert function here
+	function search( searchQuery){
+		client.connect();		
+		client.query('select doc_id from schneider_search.tags where tag= $1',[searchQuery], (err,res) => {
+			console.log("SEARCHFUNC: ");
+			console.log(err ? err.stack : res.row[0].doc_id)
+			console.log("SEARCHFUNCEND");
+		});
+	}
+  
+  
+  function search2(){
+      results = pool.query('SELECT * FROM schneider_search.tags', (err,res) =>{
+      if (err) { console.log(err.stack)
+      }else{ console.log(res.rows[0]) }
+      return res
+      pool.end()
+	  })
+  }
+  
+  function getDocIdFromTag(tag){
+    const res = await pool.query('SELECT * FROM schneider_search.tags')
+    await pool.end()
+    console.log(res)
+  }
+///////////////END DB QUERY////////////////////////
+getDocIdFromTag("testTag")
+
